6e2a4109bd8099084a467e065bb4576c8c651c93
Alex Fowler <alex@mpfowler.com>
2021-09-13 17:17:12 -0400

    Header Fix

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /*
- *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
  *  Copyright 2021 Alex Fowler
  */
 public class App
